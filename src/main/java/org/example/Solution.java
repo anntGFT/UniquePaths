82d88e1d896f3626c7c26289536c3596bccaabd2
annt <annt@gft.com>
2022-12-12 17:20:57 +0000

    Done

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,7 +1,31 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Solution {
+    public Solution(){
+
+    }
+
+    public int uniquePaths(int m, int n) {
+
+        if (m == 0) {
+
+            return 0;
+        }
+
+        int[][] aux = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) {
+
+                    aux[i][j] = 1;
+                } else {
+
+                    aux[i][j] = aux[i - 1][j] + aux[i][j - 1];
+                }
+            }
+        }
+
+        return aux[m - 1][n - 1];
     }
 }
--- src/test/java/org/example/SolutionTest.java
+++ src/test/java/org/example/SolutionTest.java
@@ -1,2 +1,49 @@
-package org.example;public class SolutionTest {
+package org.example;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SolutionTest {
+
+    static Solution solution;
+
+    int m,n,result;
+
+    @BeforeAll
+    static void setup(){
+
+        solution = new Solution();
+    }
+
+    @Test
+    public void onlyOneSpace(){
+
+        m = 1;
+        n = 1;
+        result = 1;
+
+        assertEquals(result, solution.uniquePaths(m,n));
+    }
+
+    @Test
+    public void threeColumnsTwoRows(){
+
+        m = 3;
+        n = 2;
+        result = 3;
+
+        assertEquals(result, solution.uniquePaths(m,n));
+    }
+
+    @Test
+    public void threeColumnsSevenRows(){
+
+        m = 3;
+        n = 7;
+        result = 28;
+
+        assertEquals(result, solution.uniquePaths(m,n));
+    }
 }
